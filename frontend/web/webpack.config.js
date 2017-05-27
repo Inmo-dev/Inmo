98775f0451c45c35d895550496fa6a306fcee41c
Aldrin Salazar <salazar.aldrin@gmail.com>
2017-05-26 23:40:39 -0400

    Optimalidad en transpilacion

--- frontend/web/webpack.config.js
+++ frontend/web/webpack.config.js
@@ -19,7 +19,15 @@ var config = {
         loader : 'babel-loader'
       }
     ]
+  },
+  plugins: [
+  new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
   }
+}),
+    new UglifyJSPlugin({comments:false})
+  ]
 };
 
 module.exports = config;
